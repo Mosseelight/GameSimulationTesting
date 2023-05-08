998cc662139a13aaee28041a9881c88764f31b81
Mosseelight <doctorphillip233@gmail.com>
2023-05-07 22:23:02 -0400

    Renderer Changes

--- Sim/3dRenderer/Defines.cs
+++ Sim/3dRenderer/Defines.cs
@@ -19,36 +19,36 @@ namespace GameTesting
 
     public class Triangle
     {
-        public Vertex ver1;
-        public Vertex ver2;
-        public Vertex ver3;
+        public Vertex verLeft; //left point
+        public Vertex verTop; // top point
+        public Vertex verRight; // right point
 
-        public Triangle(Vertex _ver1, Vertex _ver2, Vertex _ver3)
+        public Triangle(Vertex _verLeft, Vertex _verTop, Vertex _verRight)
         {
-            ver1 = _ver1;
-            ver2 = _ver2;
-            ver3 = _ver3;
+            verLeft = _verLeft;
+            verTop = _verTop;
+            verRight = _verRight;
         }
 
         public bool ContainsPoint(Vector2 point)
         {
-            Vector2 p1 = new Vector2(ver1.position.X, ver1.position.Y);
-            Vector2 p2 = new Vector2(ver2.position.X, ver2.position.Y);
-            Vector2 p3 = new Vector2(ver3.position.X, ver3.position.Y);
+            Vector2 p1 = new Vector2(verLeft.position.X, verLeft.position.Y);
+            Vector2 p2 = new Vector2(verTop.position.X, verTop.position.Y);
+            Vector2 p3 = new Vector2(verRight.position.X, verRight.position.Y);
 
-            Vector2 x = p3 - p1;
-            Vector2 y = p2 - p1;
-            Vector2 b = point - p1;
+            Vector2 x = p3 - p1; //c
+            Vector2 y = p2 - p1; //b
+            Vector2 b = point - p1; //p
 
             float xx = Vector2.Dot(x, x);
             float yx = Vector2.Dot(y, x);
-            float xb = Vector2.Dot(x, b);
+            float bx = Vector2.Dot(b, x);
             float yy = Vector2.Dot(y, y);
-            float yb = Vector2.Dot(y, b);
+            float by = Vector2.Dot(b, y);
 
             float denom = xx*yy - yx*yx;
-            float u = (yy * xb - yx * yb) / denom;
-            float v = (xx * yb - yx * xb) / denom;
+            float u = (yy*bx - yx*by) / denom;
+            float v = v = (xx*by - yx*bx) / denom;
 
             return (u >= 0) && (v >= 0) && (u + v < 1);
         }
--- Sim/3dRenderer/RHandler.cs
+++ Sim/3dRenderer/RHandler.cs
@@ -24,22 +24,50 @@ namespace GameTesting
 
         PixelDrawer pixelDrawer = new PixelDrawer();
 
-        Triangle triangle = new Triangle(new Vertex(new Vector3(-1, 0, 0), new Vector3(1, 1, 1), Color.Black), new Vertex(new Vector3(1, 0, 0), new Vector3(1, 1, 1), Color.Black), new Vertex(new Vector3(0, 1, 0), new Vector3(1, 1, 1), Color.Black));
+        Triangle triangle;
 
         public void InitRenderer(GraphicsDeviceManager graphics)
         {
-            pixelDrawer.visualScale = 5;
+            pixelDrawer.visualScale = 1;
             pixelDrawer.InitDrawer(graphics);
+
+            triangle = new Triangle(new Vertex(new Vector3(0, 0, 0), new Vector3(1, 1, 1), Color.Black), new Vertex(new Vector3(50 * pixelDrawer.visualScale, 100 * pixelDrawer.visualScale, 0), new Vector3(1, 1, 1), Color.Black), new Vertex(new Vector3(100 * pixelDrawer.visualScale, 50 * pixelDrawer.visualScale, 0), new Vector3(1, 1, 1), Color.Black));
         }
 
         public void Draw(Texture2D pixel, SpriteBatch spriteBatch, GraphicsDeviceManager graphics)
         {
+            triangle.verLeft.position.X++;
+            triangle.verRight.position.X++;
+            triangle.verTop.position.X++;
+            triangle.verLeft.position.Y++;
+            triangle.verRight.position.Y++;
+            triangle.verTop.position.Y++;
+            Rasterization();
             pixelDrawer.DrawPixels(pixel, spriteBatch, graphics);
+            for (int i = 0; i < pixelDrawer.colors.Length; i++)
+            {
+                pixelDrawer.colors[i] = Color.White;
+            }
         }
 
         void VertexShader()
         {
             
+        }
+
+        void Rasterization()
+        {
+            for (int x = 0; x < pixelDrawer.xTotal; x++)
+            {
+                for (int y = 0; y < pixelDrawer.yTotal; y++)
+                {
+                    Vector2 pos = pixelDrawer.GetPosOnIndex(y + pixelDrawer.yTotal * x);
+                    if(triangle.ContainsPoint(pos))
+                    {
+                        pixelDrawer.colors[y + pixelDrawer.yTotal * x] = Color.Black;
+                    }
+                }
+            }
         }
 
 
